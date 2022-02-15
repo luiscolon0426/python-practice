e30abfc0e7d8b1f3919a0934e9df5a240097f094
luiscolon0426 <luiscolon0426@gmail.com>
2022-02-15 14:44:50 +0000

    Loop practice

---
+++ Looping.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# for loop through list from 1 to 21
+
+# use modulus to checks that the result is NOT EQUAL to 0
+
+# print the odds
+
+for i in range(1, 21):
+    if ((i % 2) != 0):
+        print("i = ", i)
